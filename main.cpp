50b0c660f430ba2e2a57f11f6103b10ff60d78ee
Abbos Qurbonov <a@gmail.com>
2025-09-22 16:48:43 +0500

    all

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+// int main() {
+//     for (int i = 5; i >= 1; i--) {
+//         cout << i << endl;
+//     }
+// }
+
+// int main() {
+//     cout <<"Enter the number: ";
+//     int n;
+//     cin>>n;
+//     int i=0;
+//     int sum;
+//     while(i<n) {
+//         i++;
+//         sum= i*i;
+//         cout <<i<<" * "<<i<<" = "<<sum<<endl;
+//     }
+// }
+
+// int main() {
+//     cout << "Enter the number" ;
+//     int n;
+//     cin>>n;
+//     int maxnum = n;
+//     while (n!=0) {
+//         cout <<" Enter another number: ";
+//         cin>>n;
+//         if (maxnum<n) {
+//
+//             maxnum = n;
+//         }
+//     }
+//     if (maxnum!=0) {
+//         cout <<"The max is "<<maxnum<<endl;
+//     }
+// }
